c5dd99916117320a58d04cc0b66630dc15fcc4f9
oeddins <oeddins@lastmilehealth.org>
2016-07-21 16:13:40 +0000

    2204, 2205 exceptions to RC cohort numbering schemes.

--- views/common/view_staffIDCohortCounty.sql
+++ views/common/view_staffIDCohortCounty.sql
@@ -9,14 +9,15 @@ select
       
       case
       
-          when  ( ( ( s.n >= 1    ) and ( s.n <= 78   ) )   or 
-                  ( ( s.n >= 200  ) and ( s.n <= 1999 ) ) ) then 4
+          when  ( ( ( s.n >= 1    ) and ( s.n <= 78   ) )     or 
+                  ( ( s.n >= 200  ) and ( s.n <= 1999 ) ) )   then 4
                 
-          when  ( s.n >= 79  ) and ( s.n <= 199 )           then 5
+          when  ( s.n >= 79  ) and ( s.n <= 199 )             then 5
           
-          when  ( s.n >= 2000  ) and ( s.n <= 2199 )        then 6
+          when  ( ( ( s.n >= 2000   ) and ( s.n <= 2199 ) )   or  
+                ( (   s.n  = 2204   ) or  ( s.n  = 2205 ) ) ) then 6
           
-          when  ( s.n >= 2200  ) and ( s.n < 2699 )         then 7
+          when  ( s.n >= 2200  ) and ( s.n < 2699 )           then 7
           
           else null
           
@@ -24,14 +25,15 @@ select
       
       case
       
-          when  ( ( ( s.n >= 1    ) and ( s.n <= 78   ) )   or 
-                  ( ( s.n >= 200  ) and ( s.n <= 1999 ) ) ) then 'Konobo'
+          when  ( ( ( s.n >= 1    ) and ( s.n <= 78   ) )     or 
+                  ( ( s.n >= 200  ) and ( s.n <= 1999 ) ) )   then 'Konobo'
                 
-          when  ( s.n >= 79  ) and ( s.n <= 199 )           then 'Gboe-Ploe'
+          when  ( s.n >= 79  ) and ( s.n <= 199 )             then 'Gboe-Ploe'
           
-          when  ( s.n >= 2000  ) and ( s.n <= 2199 )        then 'Rivercess 1'
+          when  ( ( ( s.n >= 2000   ) and ( s.n <= 2199 ) )   or  
+                ( (   s.n  = 2204   ) or  ( s.n  = 2205 ) ) ) then 'Rivercess 1'
           
-          when  ( s.n >= 2200  ) and ( s.n < 2699 )         then 'Rivercess 2'
+          when  ( s.n >= 2200  ) and ( s.n < 2699 )           then 'Rivercess 2'
           
           else null
           

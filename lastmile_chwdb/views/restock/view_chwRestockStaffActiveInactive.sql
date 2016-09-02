32f54aba85d0233bbdd79e3a3b1caef25e42cd0d
oeddins <oeddins@lastmilehealth.org>
2016-09-02 14:50:36 -0600

    order by for group_concat functions
    This fixes the indeterministic behavior ot the
    view_chwRestockReport view, which drives the
    CHW Restock report

--- lastmile_chwdb/views/restock/view_chwRestockStaffActiveInactive.sql
+++ lastmile_chwdb/views/restock/view_chwRestockStaffActiveInactive.sql
@@ -15,11 +15,11 @@ select
       h.chwStatus,
       h.chwCommunityStatus,             
 
-      group_concat( distinct concat( h.community, ' (', h.communityID, ') ' ) order by h.community separator ', '  ) as communityAndIDList,
-      group_concat( distinct h.district                 separator ', '  ) as districtList,
-      group_concat( distinct h.healthFacility           separator ', '  ) as healthFacilityList,
-      group_concat( distinct h.healthDistrict           separator ', '  ) as healthDistrictList,
-      group_concat( distinct h.county                   separator ', '  ) as countyList
+      group_concat( distinct concat( h.community, ' (', h.communityID, ') ' ) order by h.community, h.communityID separator ', '  ) as communityAndIDList,
+      group_concat( distinct h.district                                       order by h.district                 separator ', '  ) as districtList,
+      group_concat( distinct h.healthFacility                                 order by h.healthFacility           separator ', '  ) as healthFacilityList,
+      group_concat( distinct h.healthDistrict                                 order by h.healthDistrict           separator ', '  ) as healthDistrictList,
+      group_concat( distinct h.county                                         order by h.county                   separator ', '  ) as countyList
       
 from view_staffSupervisionCommunityHistory as h
 where   not ( ( ( chwlID is null ) or ( trim( chwlID ) like '' ) ) or

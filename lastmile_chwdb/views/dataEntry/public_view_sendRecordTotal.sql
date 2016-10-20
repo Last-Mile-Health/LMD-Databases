c8ef048ebf7ea2a84a0697e7428f386273a2d1b3
oeddins <oeddins@lastmilehealth.org>
2016-10-20 10:21:30 +0000

    Added web to odk totals

--- lastmile_chwdb/views/dataEntry/public_view_sendRecordTotal.sql
+++ lastmile_chwdb/views/dataEntry/public_view_sendRecordTotal.sql
@@ -2,135 +2,187 @@ use lastmile_chwdb;
 
 drop view if exists public_view_sendRecordTotal;
 
-create view public_view_sendRecordTotal as 
+create view public_view_sendRecordTotal as
+
+select
+      'web'                                                   as formType,
+      'Sick Child'                                            as formName,
+      if( fhwID >= 2000, 'Rivercess', 'Grand Gedeh' )         as county,
+      year( meta_insertDatetime )                             as yearRecord,
+      month( meta_insertDatetime )                            as monthRecord,
+      count( * )                                              as recordCount
+from lastmile_db.tbl_data_fhw_sch_sickchild
+where not ( ( meta_insertDatetime is null ) or ( trim( meta_insertDatetime ) like '' ) )
+group by formType, formName, county, yearRecord, monthRecord
+
+union all
+
+select
+      'web'                                                   as formType,
+      'Health Survey'                                         as formName,
+      if( chwID >= 2000, 'Rivercess', 'Grand Gedeh' )         as county,
+      year( meta_insertDatetime )                             as yearRecord,
+      month( meta_insertDatetime )                            as monthRecord,
+      count( * )                                              as recordCount
+from staging_kpiAssessmentStep1
+where not ( ( meta_insertDatetime is null ) or ( trim( meta_insertDatetime ) like '' ) )
+group by formType, formName, county, yearRecord, monthRecord
+
+union all
+
+select
+      'web'                                                   as formType,
+      'Household Registration'                                as formName,
+      if( chwID >= 2000, 'Rivercess', 'Grand Gedeh' )         as county,
+      year( meta_insertDatetime )                             as yearRecord,
+      month( meta_insertDatetime )                            as monthRecord,
+      count( * )                                              as recordCount
+from staging_householdRegistrationStep1
+where not ( ( meta_insertDatetime is null ) or ( trim( meta_insertDatetime ) like '' ) )
+group by formType, formName, county, yearRecord, monthRecord
+
+union all
+
+select
+      'web'                                                   as formType,
+      'CHA Monthly Service Report'                            as formName,
+      if( chwlID >= 2000, 'Rivercess', 'Grand Gedeh' )        as county,
+      year( meta_insertDatetime )                             as yearRecord,
+      month( meta_insertDatetime )                            as monthRecord,
+      count( * )                                              as recordCount
+from staging_chwMonthlyServiceReportStep1
+where not ( ( meta_insertDatetime is null ) or ( trim( meta_insertDatetime ) like '' ) )
+group by formType, formName, county, yearRecord, monthRecord
+
+union all
 
 select
       'odk'                                                   as formType,
-      'arrival log'                                           as formName,
+      'Arrival Log'                                           as formName,
       if( chwlID >= 2000, 'Rivercess', 'Grand Gedeh' )        as county,
       year( meta_insertDatetime )                             as yearRecord,
       month( meta_insertDatetime )                            as monthRecord,
       count( * )                                              as recordCount
 from staging_odk_arrivalchecklog
 where not ( ( meta_insertDatetime is null ) or ( trim( meta_insertDatetime ) like '' ) )
-group by county, yearRecord, monthRecord
+group by formType, formName, county, yearRecord, monthRecord
 
 union all
 
 select
       'odk'                                                   as formType,
-      'departure log'                                         as formName,
+      'Departure Log'                                         as formName,
       if( chwlID >= 2000, 'Rivercess', 'Grand Gedeh' )        as county,
       year( meta_insertDatetime )                             as yearRecord,
       month( meta_insertDatetime )                            as monthRecord,
       count( * )                                              as recordCount
 from staging_odk_departurechecklog
 where not ( ( meta_insertDatetime is null ) or ( trim( meta_insertDatetime ) like '' ) )
-group by county, yearRecord, monthRecord
+group by formType, formName, county, yearRecord, monthRecord
 
 union all
 
 select
       'odk'                                                   as formType,
-      'chw restock'                                           as formName,
+      'CHA Restock'                                           as formName,
       if( chwlID >= 2000, 'Rivercess', 'Grand Gedeh' )        as county,
       year( meta_insertDatetime )                             as yearRecord,
       month( meta_insertDatetime )                            as monthRecord,
       count( * )                                              as recordCount
 from staging_odk_chwrestock
 where not ( ( meta_insertDatetime is null ) or ( trim( meta_insertDatetime ) like '' ) )
-group by county, yearRecord, monthRecord
+group by formType, formName, county, yearRecord, monthRecord
 
 union all
 
 select
       'odk'                                                   as formType,
-      'chw restock archive'                                   as formName,
+      'CHA Restock Archive'                                   as formName,
       if( chwlID >= 2000, 'Rivercess', 'Grand Gedeh' )        as county,
       year( meta_insertDatetime )                             as yearRecord,
       month( meta_insertDatetime )                            as monthRecord,
       count( * )                                              as recordCount
 from staging_odk_chwrestock_archive
 where not ( ( meta_insertDatetime is null ) or ( trim( meta_insertDatetime ) like '' ) )
-group by county, yearRecord, monthRecord
+group by formType, formName, county, yearRecord, monthRecord
 
 union all
 
 select
       'odk'                                                   as formType,
-      'chw supervision report'                                as formName,
+      'CHA Supervision Report'                                as formName,
       if( chwlID >= 2000, 'Rivercess', 'Grand Gedeh' )        as county,
       year( meta_insertDate )                                 as yearRecord,
       month( meta_insertDate )                                as monthRecord,
       count( * )                                              as recordCount
 from staging_odk_chwsupervisionreport
 where not ( ( meta_insertDate is null ) or ( trim( meta_insertDate ) like '' ) )
-group by county, yearRecord, monthRecord
+group by formType, formName, county, yearRecord, monthRecord
 
 union all
 
 select
       'odk'                                                   as formType,
-      'health survey'                                         as formName,
+      'Health Survey'                                         as formName,
       if( chwID >= 2000, 'Rivercess', 'Grand Gedeh' )         as county,
       year( meta_insertDatetime )                             as yearRecord,
       month( meta_insertDatetime )                            as monthRecord,
       count( * )                                              as recordCount
 from staging_odk_healthsurvey
 where not ( ( meta_insertDatetime is null ) or ( trim( meta_insertDatetime ) like '' ) )
-group by county, yearRecord, monthRecord
+group by formType, formName, county, yearRecord, monthRecord
 
 union all
 
 select
       'odk'                                                   as formType,
-      'routine visit'                                         as formName,
+      'Routine Visit'                                         as formName,
       if( chwID >= 2000, 'Rivercess', 'Grand Gedeh' )         as county,
       year( meta_insertDatetime )                             as yearRecord,
       month( meta_insertDatetime )                            as monthRecord,
       count( * )                                              as recordCount
 from staging_odk_routinevisit
 where not ( ( meta_insertDatetime is null ) or ( trim( meta_insertDatetime ) like '' ) )
-group by county, yearRecord, monthRecord
+group by formType, formName, county, yearRecord, monthRecord
 
 union all
 
 select
       'odk'                                                   as formType,
-      'routine visit archive'                                 as formName,
+      'Routine Visit Archive'                                 as formName,
       if( chwID >= 2000, 'Rivercess', 'Grand Gedeh' )         as county,
       year( meta_insertDate )                                 as yearRecord,
       month( meta_insertDate )                                as monthRecord,
       count( * )                                              as recordCount
 from staging_odk_routinevisit_archive
 where not ( ( meta_insertDate is null ) or ( trim( meta_insertDate ) like '' ) )
-group by county, yearRecord, monthRecord
+group by formType, formName, county, yearRecord, monthRecord
 
 union all
 
 select
       'odk'                                                   as formType,
-      'sick child'                                            as formName,
+      'Sick Child'                                            as formName,
       if( chwID >= 2000, 'Rivercess', 'Grand Gedeh' )         as county,
       year( meta_insertDatetime )                             as yearRecord,
       month( meta_insertDatetime )                            as monthRecord,
       count( * )                                              as recordCount
 from staging_odk_sickChildForm
 where not ( ( meta_insertDatetime is null ) or ( trim( meta_insertDatetime ) like '' ) )
-group by county, yearRecord, monthRecord
+group by formType, formName, county, yearRecord, monthRecord
 
 union all
 
 select
       'odk'                                                   as formType,
-      'supervision visit log'                                 as formName,
+      'Supervision Visit Log'                                 as formName,
       if( ccsID >= 2000, 'Rivercess', 'Grand Gedeh' )         as county,
       year( meta_insertDatetime )                             as yearRecord,
       month( meta_insertDatetime )                            as monthRecord,
       count( * )                                              as recordCount
 from staging_odk_supervisionvisitlog
 where not ( ( meta_insertDatetime is null ) or ( trim( meta_insertDatetime ) like '' ) )
-group by county, yearRecord, monthRecord
+group by formType, formName, county, yearRecord, monthRecord
 
 union all
 
@@ -143,7 +195,7 @@ select
       count( * )                                              as recordCount
 from staging_odk_vaccinetracker
 where not ( ( meta_insertDatetime is null ) or ( trim( meta_insertDatetime ) like '' ) )
-group by county, yearRecord, monthRecord
+group by formType, formName, county, yearRecord, monthRecord
 ;
 
 

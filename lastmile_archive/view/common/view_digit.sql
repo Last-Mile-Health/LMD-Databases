c49c2c90551af6c839c1d89b08c004078bd2b084
oeddins <oeddins@lastmilehealth.org>
2017-06-28 10:39:35 +0000

    Move "common" views to archive

---
+++ lastmile_archive/view/common/view_digit.sql
@@ -0,0 +1,25 @@
+use lastmile_archive;
+
+drop view if exists view_digit;
+
+create view view_digit as 
+select 0 as digit
+union
+select 1 as digit
+union
+select 2 as digit
+union
+select 3 as digit
+union
+select 4 as digit
+union
+select 5 as digit
+union
+select 6 as digit
+union
+select 7 as digit
+union
+select 8 as digit
+union
+select 9 as digit
+;
---
+++ lastmile_archive/view/common/view_sequentialUnsignedInteger.sql
@@ -0,0 +1,11 @@
+use lastmile_archive;
+
+drop view if exists view_sequentialUnsignedInteger;
+
+create view view_sequentialUnsignedInteger as
+select d4.digit * 1000 + d3.digit * 100 + d2.digit * 10 + d1.digit as n
+from view_digit as d1
+    cross join view_digit as d2
+        cross join view_digit as d3
+            cross join view_digit as d4
+order by n asc;

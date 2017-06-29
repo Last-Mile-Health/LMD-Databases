7922b2fcb292d08c40e98b6c8cbb88e822705a58
oeddins <oeddins@lastmilehealth.org>
2017-06-29 10:27:42 +0000

    view_tbl_utility_dataUploadDebugging
    Simple view for extracting query string for debug.

---
+++ lastmile_dataportal/views/view_tbl_utility_dataUploadDebugging.sql
@@ -0,0 +1,6 @@
+use lastmile_dataportal;
+
+drop view if exists view_tbl_utility_dataUploadDebugging;
+
+create view view_tbl_utility_dataUploadDebugging as
+select queryString from tbl_utility_dataUploadDebugging;

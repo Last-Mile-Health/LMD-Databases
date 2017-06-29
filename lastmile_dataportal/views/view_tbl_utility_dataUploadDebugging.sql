55f9d4f47fb3bb3fe0280279bef1193b79a09117
oeddins <oeddins@lastmilehealth.org>
2017-06-29 10:33:37 +0000

    tweaks

--- lastmile_dataportal/views/view_tbl_utility_dataUploadDebugging.sql
+++ lastmile_dataportal/views/view_tbl_utility_dataUploadDebugging.sql
@@ -3,4 +3,5 @@ use lastmile_dataportal;
 drop view if exists view_tbl_utility_dataUploadDebugging;
 
 create view view_tbl_utility_dataUploadDebugging as
-select queryString from tbl_utility_dataUploadDebugging;
+select queryString
+from tbl_utility_dataUploadDebugging;

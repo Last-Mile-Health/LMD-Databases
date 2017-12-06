1b1a7590cbd8efd86b625251ef4736bcd5afa9a6
Owen Eddins <oeddins@lastmilehealth.org>
2017-12-06 15:12:12 -0700

    NCHAP changes

--- lastmile_cha/view/view_community_cha.sql
+++ lastmile_cha/view/view_community_cha.sql
@@ -6,15 +6,10 @@ create view view_community_cha as
 
 select
       pc.community_id,
-
-      group_concat( pc.position_id  order by cast( pc.position_id as unsigned ) separator ', ' )  as position_id_list,
-      count( pc.position_id )                                                                     as position_count,
-      
-      group_concat( substring_index( pc.person_id, '|', 1 ) order by cast( substring_index( pc.person_id, '|', 1 ) as unsigned ) separator ', ' ) as cha_id_list,
-      count( substring_index( pc.person_id, '|', 1 ) ) as cha_count
-      
+      group_concat( pc.position_id  order by pc.position_id separator ', ' )  as position_id_list,
+      count( pc.position_id )                                                 as position_count,
+      group_concat( pc.person_id    order by pc.person_id   separator ', ' )  as person_id_list,
+      count( pc.person_id )                                                   as person_count   
 from view_position_community_cha as pc
 group by pc.community_id
 ;
-      
-

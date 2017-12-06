58d8da0ce0aec2d2111d65917e14975fed975e9d
Owen Eddins <oeddins@lastmilehealth.org>
2017-12-06 14:25:07 -0700

    Bug fix view_history_position_person_chss

--- lastmile_cha/view/view_history_position_person_chss.sql
+++ lastmile_cha/view/view_history_position_person_chss.sql
@@ -6,9 +6,10 @@ create view view_history_position_person_chss as
 
 select
 
-      substring_index( trim( l.person_id ), '|', 1 )            as chss_id,
+      trim( l.position_id )                                     as chss_id,
       concat( trim( r.first_name ), ' ', trim( r.last_name ) )  as chss,
       trim( l.position_id )                                     as position_id,
+      trim( l.person_id )                                       as person_id,    
       l.begin_date                                              as position_person_begin_date,
       l.end_date                                                as position_person_end_date
       

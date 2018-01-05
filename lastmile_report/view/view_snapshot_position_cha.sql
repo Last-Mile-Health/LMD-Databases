d9d90f409e8689857b415d661f311220e6626d6a
Owen Eddins <oeddins@lastmilehealth.org>
2018-01-04 17:21:35 -0700

    Changed sums for cohorts to counties

--- lastmile_report/view/view_snapshot_position_cha.sql
+++ lastmile_report/view/view_snapshot_position_cha.sql
@@ -7,13 +7,16 @@ create view view_snapshot_position_cha as
 select
       position_status, 
       snapshot_date, 
-      cohort,
+      -- cohort,
+      county,
       count( * )                                                    as cha_count,
       round( sum( position_community_count_proportional ), 0 )      as community_count,
       sum( population )                                             as population,
       sum( household)                                               as household 
 
-from data_mart_snapshot_position_cha 
-group by position_status, snapshot_date, cohort 
-order by position_status, snapshot_date, cohort
+from data_mart_snapshot_position_cha
+group by position_status, snapshot_date, county 
+order by position_status, snapshot_date, county
+-- group by position_status, snapshot_date, cohort 
+-- order by position_status, snapshot_date, cohort
 ;

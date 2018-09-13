a7585d80f55523cec96205b7ae7ea76d9fd64c95
Owen Eddins <oeddins@lastmilehealth.org>
2018-09-13 12:30:29 -0600

    Recode view_qao_supervision_rate_county.sql

--- lastmile_report/view/view_qao_supervision_rate_county.sql
+++ lastmile_report/view/view_qao_supervision_rate_county.sql
@@ -5,17 +5,26 @@ drop view if exists lastmile_report.view_qao_supervision_rate_county;
 create view lastmile_report.view_qao_supervision_rate_county as
 
 select
-      trim( substring_index( c.county_list, ',', 1 ) )              as county,
-      if( t.territory_id like '1\\_6', '6_31', t.territory_id )     as territory_id, -- case of Grand Gedeh use 6_31 GG (LMH)
-      year(  q.TodayDate )                                          as year_reported,
-      month( q.TodayDate )                                          as month_reported,
-      count( * )                                                    as number_supervision_visit
+      /*  
+          Hard code the territories rather than join with view_territories.  GG will need to be manually mapped to 
+          6_31 GG (LMH) anyway.
+      */
+      case substring_index( c.position_id, '-', 1 )
+            when 'GG' then '6_31'
+            when 'GB' then '1_4'
+            when 'RI' then '1_14'
+            else null
+      end                                                   as territory_id,
+      
+      year(  q.TodayDate )                                  as year_reported,
+      month( q.TodayDate )                                  as month_reported,
+      count( * )                                            as number_supervision_visit
+      
 from lastmile_upload.odk_QAOSupervisionChecklistForm as q
     left outer join lastmile_cha.view_position_qao_person_geo as c on trim( q.QAOID ) like c.position_id
-        left outer join lastmile_dataportal.view_territories as t on ( trim( substring_index( c.county_list, ',', 1 ) ) like t.territory_name ) and ( 'county' like t.territory_type )
+where not( c.position_id is null )
 group by 
-        trim( substring_index( c.county_list, ',', 1 ) ),
-        t.territory_id,
+        substring_index( position_id, '-', 1 ),
         year( q.TodayDate ), 
         month( q.TodayDate )
 ;

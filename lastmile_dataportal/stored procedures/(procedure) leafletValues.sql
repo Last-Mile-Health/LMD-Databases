6fd8bf09491ab129f06fe4a3ee6647852c694ac7
Owen Eddins <oeddins@lastmilehealth.org>
2018-11-13 13:36:38 -0700

    Leaflet: CHA/CHSS counts, dynamically generate

--- lastmile_dataportal/stored procedures/(procedure) leafletValues.sql
+++ lastmile_dataportal/stored procedures/(procedure) leafletValues.sql
@@ -273,37 +273,87 @@ group by county_id
 ;
 
 
+/*
 -- 28. Number of CHAs deployed (LMH)
 REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`ind_id`,`territory_id`,`period_id`,`value`)
 SELECT 28, CONCAT('2_',health_district_id), 1, COUNT(*) FROM lastmile_cha.view_base_cha GROUP BY health_district_id
 UNION SELECT 28, CONCAT('1_',county_id), 1, COUNT(*) FROM lastmile_cha.view_base_cha GROUP BY county_id;
 
-
 -- 28. Number of CHAs deployed (NCHA)
--- !!!!! temporary !!!!! 
+
 REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_1',1,28,107);
-REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_2',1,28,229);
+REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_2',1,28,238);
 REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_3',1,28,160);
-REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_4',1,28,0);
-REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_5',1,28,122);
+REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_4',1,28,101);
+REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_5',1,28,120);
 REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_6',1,28,219);
 REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_7',1,28,129);
-REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_8',1,28,357);
+REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_8',1,28,353);
 REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_9',1,28,110);
-REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_10',1,28,114);
+REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_10',1,28,113);
 REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_11',1,28,0);
-REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_12',1,28,770);
-REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_13',1,28,150);
-REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_14',1,28,213);
-REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_15',1,28,193);
+REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_12',1,28,767);
+REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_13',1,28,147);
+REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_14',1,28,259);
+REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`territory_id`,`period_id`,`ind_id`,`value`) VALUES ('1_15',1,28,195);
+
+*/
+
+-- 28. Number of CHAs deployed
+
+replace into lastmile_dataportal.tbl_values_leaflet ( territory_id, period_id, ind_id, value ) 
+
+-- Assisted Areas
+select t.territory_id, 1 as period_id, 28, s.cha_number_active
+from lastmile_dataportal.tbl_nchap_scale_chss_cha as s
+    left outer join lastmile_dataportal.view_territories as t on  ( trim( s.county )          like trim( t.territory_name ) ) and 
+                                                                  ( trim( t.territory_type )  like 'county' )
+where not ( s.county like '%Grand%Bassa%' or s.county like '%Grand%Gedeh%' or s.county like '%Rivercess%' )
+
+union all
+
+-- LMH Managed Areas
+select s.territory_id, 1 as period_id, 28, s.num_cha 
+from lastmile_report.mart_program_scale as s
+where s.territory_id like '1\\_%'
+
+union all
+
+select concat('2_',health_district_id), 1 as period_id, 28, count( * ) 
+from lastmile_cha.view_base_cha 
+group by health_district_id
+;
 
 
 -- 29. Number of CHSSs deployed
--- !!!!! check if this is still true !!!!! Note: the second SELECT is disabled because it is manually being overwritten by NCHA scale numbers !!!!!
+/*
 REPLACE INTO lastmile_dataportal.tbl_values_leaflet (`ind_id`,`territory_id`,`period_id`,`value`)
 SELECT 29, CONCAT('2_',health_district_id), 1, COUNT(*) FROM lastmile_cha.view_base_chss GROUP BY health_district_id
 UNION SELECT 29, CONCAT('1_',county_id), 1, COUNT(*) FROM lastmile_cha.view_base_chss GROUP BY county_id;
+*/
 
+replace into lastmile_dataportal.tbl_values_leaflet ( territory_id, period_id, ind_id, value ) 
+
+-- Assisted Areas
+select t.territory_id, 1 as period_id, 29, s.chss_number_active
+from lastmile_dataportal.tbl_nchap_scale_chss_cha as s
+    left outer join lastmile_dataportal.view_territories as t on  ( trim( s.county )          like trim( t.territory_name ) ) and 
+                                                                  ( trim( t.territory_type )  like 'county' )
+where not ( s.county like '%Grand%Bassa%' or s.county like '%Grand%Gedeh%' or s.county like '%Rivercess%' )
+
+union all
+
+-- LMH Managed Areas
+select s.territory_id, 1 as period_id, 29, s.num_chss 
+from lastmile_report.mart_program_scale as s
+where s.territory_id like '1\\_%'
+
+union all
+
+select concat('2_',health_district_id), 1 as period_id, 29, count( * ) 
+from lastmile_cha.view_base_chss 
+group by health_district_id
+;
 
 
 

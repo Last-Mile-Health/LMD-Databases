757df6c2fc3b698d94b12b3c6d79315cf538ac4e
Owen Eddins <oeddins@lastmilehealth.org>
2019-01-09 16:40:21 -0700

    feat: update nchap id, include chss CTR code

--- lastmile_upload/stored_procedure/upload_update_nchap_id.sql
+++ lastmile_upload/stored_procedure/upload_update_nchap_id.sql
@@ -54,6 +54,8 @@ update lastmile_upload.de_case_scenario                 set cha_id_inserted_form
 
 update lastmile_upload.de_case_scenario                 set chss_id_inserted_format           = lastmile_upload.nchap_id_format( chss_id_inserted );
 
+update lastmile_upload.de_chss_case_scenario            set chss_id_inserted_format           = lastmile_upload.nchap_id_format( chss_id_inserted );
+
 update lastmile_upload.de_chaHouseholdRegistration      set cha_id_inserted_format            = lastmile_upload.nchap_id_format( cha_id_inserted );
 
 update lastmile_upload.de_chaHouseholdRegistration      set chss_id_inserted_format           = lastmile_upload.nchap_id_format( chss_id_inserted );
@@ -158,6 +160,19 @@ where ( trim( a.chss_id_inserted_format ) like m.position_id ) or ( trim( a.chss
 
 
 insert into lastmile_upload.log_update_nchap_id ( meta_date_time, table_name ) values ( now(), 'de_case_scenario' );
+
+
+-- de_chss_case_scenario --------------------------------------- 2.
+
+update lastmile_upload.de_chss_case_scenario a, lastmile_cha.view_base_history_moh_lmh_chss_id m
+
+    set a.chss_id = if( m.chss_id_historical is null, trim( a.chss_id_inserted_format ), m.position_id )
+    
+where ( trim( a.chss_id_inserted_format ) like m.position_id ) or ( trim( a.chss_id_inserted_format ) like m.chss_id_historical )
+;
+
+
+insert into lastmile_upload.log_update_nchap_id ( meta_date_time, table_name ) values ( now(), 'de_chss_case_scenario' );
 
 
 -- de_chaHouseholdRegistration --------------------------------------- 2.

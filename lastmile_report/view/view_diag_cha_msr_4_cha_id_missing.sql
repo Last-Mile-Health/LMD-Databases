ba2d876c5ad57daff80fa31f7c5dce80e40fa945
Owen Eddins <oeddins@lastmilehealth.org>
2019-04-17 15:17:04 -0600

    bug: change month/year conditional clause from varchar to unsigned comparision

--- lastmile_report/view/view_diag_cha_msr_4_cha_id_missing.sql
+++ lastmile_report/view/view_diag_cha_msr_4_cha_id_missing.sql
@@ -25,9 +25,9 @@ select
       
 from lastmile_report.view_twelve_month as d
     cross join lastmile_cha.view_base_position_cha as v
-        left outer join lastmile_upload.de_cha_monthly_service_report as m on ( trim( v.position_id    ) like trim( m.cha_id )          ) and
-                                                                              ( trim( d.year_reported  ) like trim( m.year_reported )   ) and
-                                                                              ( trim( d.month_reported ) like trim( m.month_reported )  )
+        left outer join lastmile_upload.de_cha_monthly_service_report as m on ( trim( v.position_id ) like trim( m.cha_id )                                   ) and
+                                                                              ( cast( d.year_reported   as unsigned ) = cast( m.year_reported   as unsigned ) ) and
+                                                                              ( cast( d.month_reported  as unsigned ) = cast( m.month_reported  as unsigned ) )
 where ( ( v.cohort is null ) or  not ( v.cohort like '%UNICEF%' ) ) and ( m.cha_id is null ) 
 
 order by cast( d.year_reported as unsigned ) desc, cast( d.month_reported as unsigned ) desc, v.county asc, v.health_district asc, v.health_facility asc, v.chss_position_id asc, v.position_id

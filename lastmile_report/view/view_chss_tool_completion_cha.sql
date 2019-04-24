cbd2fd971ab43ebb0a78209b45bf41c455b26610
Owen Eddins <oeddins@lastmilehealth.org>
2019-04-24 17:33:32 -0600

    bug: use view_base_cha_basic_info

---
+++ lastmile_report/view/view_chss_tool_completion_cha.sql
@@ -0,0 +1,14 @@
+use lastmile_report;
+
+drop view if exists lastmile_report.view_chss_tool_completion_cha;
+
+create view lastmile_report.view_chss_tool_completion_cha as 
+
+select 
+        a.chss_position_id    as chss_id,
+        count( a.cha )        as num_cha
+        
+from lastmile_cha.view_base_cha_basic_info as a
+where not( a.position_id is null )
+group by a.chss_position_id
+;

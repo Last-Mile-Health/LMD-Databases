36b0222a67547732132573839892ab347f516afb
Owen Eddins <oeddins@lastmilehealth.org>
2019-09-13 16:18:13 +0200

    bug: cast month to unsigned int so 01 and 1 are equal

---
+++ lastmile_report/view/view_chss_tool_completion_msr_cha.sql
@@ -0,0 +1,15 @@
+use lastmile_report;
+
+drop view if exists lastmile_report.view_chss_tool_completion_msr_cha;
+
+create view lastmile_report.view_chss_tool_completion_msr_cha as 
+select  
+      trim( chss_id )                     as chss_id,
+      cast( month_reported  as unsigned ) as month_reported,
+      cast( year_reported   as unsigned ) as year_reported,
+      sum( 1 )                            as num_cha_msrs
+from lastmile_upload.de_cha_monthly_service_report
+group by  trim( chss_id ),
+          cast( month_reported  as unsigned ),
+          cast( year_reported   as unsigned )
+;

37691f2689950b3340c485606d0c8328fb3de1cf
Owen Eddins <oeddins@lastmilehealth.org>
2019-10-17 14:56:32 +0200

    feat: CHSS restock act25, act5, ors, amox indicators

--- lastmile_dataportal/stored procedures/(procedure) dataPortalValues.sql
+++ lastmile_dataportal/stored procedures/(procedure) dataPortalValues.sql
@@ -789,6 +789,7 @@ where not ( county_id is null ) and
  *
 */
 
+/*
 if @p_month = 6 then
 
 replace into lastmile_dataportal.tbl_values (`ind_id`,`territory_id`,`period_id`,`month`,`year`,`value`)
@@ -835,6 +836,8 @@ from (
 ) as a;
 
 end if;
+
+*/
 
 -- 17. Number of attempted supervision visits
 -- Currently based off of ODK data
@@ -4569,6 +4572,162 @@ where
         ( `year`=@p_yearMinus2  and `month`=@p_monthMinus2  )
       )
 ;
+
+
+/*
+ * 800.	Percent of CHSSs with ACT-25mg in stock	percent	Restock
+ * 801.	Percent of CHSSs with ACT-50mg in stock	percent	Restock
+ * 802.	Percent of CHSSs with ORS in stock
+ * 803.	Percent of CHSSs with Amoxicillin-250mg in stock
+ * 804.	Percent of CHSSs with ACT-25mg, ACT-50mg, ORS, and Amoxicillin-250mg in stock  
+ *
+ * 1. CHSS are restocked once a month.
+ * 2. For denominator use the number of CHSSs who are active for the county for the month in database.
+ * 3. For numerator count a CHSS (position_id) as having a commodity in stock if there is one or more 
+ *    in stock on day of restock.
+ * 4. If a chss (position_id) has more than a single restock in a month take the lowest value and count
+ *    as having a commodity in stock if there is one or more in stock on day of restock.
+ * 5. There could be undercounting or over counting if the number of CHSSs in the database is high or low.
+ * 6  If there is any ORS and amox 250 and ( ACT 25 or ACT 50 ) is present, then 804 is considered to have stock.
+ *
+*/
+
+replace into lastmile_dataportal.tbl_values ( ind_id, territory_id, period_id, `month`, `year`, value )
+select  800, a.territory_id, 1 as period_id, @p_month, @p_year, 
+        round( sum( if( a.value > 0, 1, 0 ) ) / s.num_chss, 3 ) as value  
+from ( 
+        select territory_id, chss_id, min( coalesce( act_25_initial_stock_on_hand, 0 ) ) as value
+        from lastmile_report.mart_view_base_restock_chss as r
+        where not ( territory_id is null ) and ( restock_month = @p_month ) and ( restock_year = @p_year )
+        group by territory_id, chss_id       
+) as a
+    left outer join lastmile_report.mart_program_scale as s on a.territory_id like s.territory_id
+group by a.territory_id
+
+union all
+
+select  800, '6_16', 1 as period_id, @p_month, @p_year, 
+
+        round( sum( if( a.value > 0, 1, 0 ) ) / sum( distinct s.num_chss ), 3 ) as value  
+from ( 
+        select territory_id, chss_id, min( coalesce( act_25_initial_stock_on_hand, 0 ) ) as value
+        from lastmile_report.mart_view_base_restock_chss as r
+        where not ( territory_id is null ) and ( restock_month = @p_month ) and ( restock_year = @p_year )
+        group by chss_id       
+) as a
+    left outer join lastmile_report.mart_program_scale as s on a.territory_id like s.territory_id
+;
+
+
+replace into lastmile_dataportal.tbl_values ( ind_id, territory_id, period_id, `month`, `year`, value )
+select  801, a.territory_id, 1 as period_id, @p_month, @p_year, 
+        round( sum( if( a.value > 0, 1, 0 ) ) / s.num_chss, 3 ) as value  
+from ( 
+        select territory_id, chss_id, min( coalesce( act_50_initial_stock_on_hand, 0 ) ) as value
+        from lastmile_report.mart_view_base_restock_chss as r
+        where not ( territory_id is null ) and ( restock_month = @p_month ) and ( restock_year = @p_year )
+        group by territory_id, chss_id       
+) as a
+    left outer join lastmile_report.mart_program_scale as s on a.territory_id like s.territory_id
+group by a.territory_id
+
+union all
+
+select  801, '6_16', 1 as period_id, @p_month, @p_year, 
+
+        round( sum( if( a.value > 0, 1, 0 ) ) / sum( distinct s.num_chss ), 3 ) as value  
+from ( 
+        select territory_id, chss_id, min( coalesce( act_50_initial_stock_on_hand, 0 ) ) as value
+        from lastmile_report.mart_view_base_restock_chss as r
+        where not ( territory_id is null ) and ( restock_month = @p_month ) and ( restock_year = @p_year )
+        group by chss_id       
+) as a
+    left outer join lastmile_report.mart_program_scale as s on a.territory_id like s.territory_id
+;
+
+
+replace into lastmile_dataportal.tbl_values ( ind_id, territory_id, period_id, `month`, `year`, value )
+select  802, a.territory_id, 1 as period_id, @p_month, @p_year, 
+        round( sum( if( a.value > 0, 1, 0 ) ) / s.num_chss, 3 ) as value  
+from ( 
+        select territory_id, chss_id, min( coalesce( ors_initial_stock_on_hand, 0 ) ) as value
+        from lastmile_report.mart_view_base_restock_chss as r
+        where not ( territory_id is null ) and ( restock_month = @p_month ) and ( restock_year = @p_year )
+        group by territory_id, chss_id       
+) as a
+    left outer join lastmile_report.mart_program_scale as s on a.territory_id like s.territory_id
+group by a.territory_id
+
+union all
+
+select  802, '6_16', 1 as period_id, @p_month, @p_year, 
+
+        round( sum( if( a.value > 0, 1, 0 ) ) / sum( distinct s.num_chss ), 3 ) as value  
+from ( 
+        select territory_id, chss_id, min( coalesce( ors_initial_stock_on_hand, 0 ) ) as value
+        from lastmile_report.mart_view_base_restock_chss as r
+        where not ( territory_id is null ) and ( restock_month = @p_month ) and ( restock_year = @p_year )
+        group by chss_id       
+) as a
+    left outer join lastmile_report.mart_program_scale as s on a.territory_id like s.territory_id
+;
+
+
+replace into lastmile_dataportal.tbl_values ( ind_id, territory_id, period_id, `month`, `year`, value )
+select  803, a.territory_id, 1 as period_id, @p_month, @p_year, 
+        round( sum( if( a.value > 0, 1, 0 ) ) / s.num_chss, 3 ) as value  
+from ( 
+        select territory_id, chss_id, min( coalesce( amoxicillin_250_initial_stock_on_hand, 0 ) ) as value
+        from lastmile_report.mart_view_base_restock_chss as r
+        where not ( territory_id is null ) and ( restock_month = @p_month ) and ( restock_year = @p_year )
+        group by territory_id, chss_id       
+) as a
+    left outer join lastmile_report.mart_program_scale as s on a.territory_id like s.territory_id
+group by a.territory_id
+
+union all
+
+select  803, '6_16', 1 as period_id, @p_month, @p_year, 
+
+        round( sum( if( a.value > 0, 1, 0 ) ) / sum( distinct s.num_chss ), 3 ) as value  
+from ( 
+        select territory_id, chss_id, min( coalesce( amoxicillin_250_initial_stock_on_hand, 0 ) ) as value
+        from lastmile_report.mart_view_base_restock_chss as r
+        where not ( territory_id is null ) and ( restock_month = @p_month ) and ( restock_year = @p_year )
+        group by chss_id       
+) as a
+    left outer join lastmile_report.mart_program_scale as s on a.territory_id like s.territory_id
+;
+
+replace into lastmile_dataportal.tbl_values ( ind_id, territory_id, period_id, `month`, `year`, value )
+select  804, a.territory_id, 1 as period_id, @p_month, @p_year, 
+        round( sum( if( a.value > 0, 1, 0 ) ) / s.num_chss, 3 ) as value  
+from ( 
+        select territory_id, chss_id, min( coalesce( important_initial_stock_on_hand, 0 ) ) as value
+        from lastmile_report.mart_view_base_restock_chss as r
+        where not ( territory_id is null ) and ( restock_month = @p_month ) and ( restock_year = @p_year )
+        group by territory_id, chss_id       
+) as a
+    left outer join lastmile_report.mart_program_scale as s on a.territory_id like s.territory_id
+group by a.territory_id
+
+union all
+
+select  804, '6_16', 1 as period_id, @p_month, @p_year, 
+
+        round( sum( if( a.value > 0, 1, 0 ) ) / sum( distinct s.num_chss ), 3 ) as value  
+from ( 
+        select territory_id, chss_id, min( coalesce( important_initial_stock_on_hand, 0 ) ) as value
+        from lastmile_report.mart_view_base_restock_chss as r
+        where not ( territory_id is null ) and ( restock_month = @p_month ) and ( restock_year = @p_year )
+        group by chss_id       
+) as a
+    left outer join lastmile_report.mart_program_scale as s on a.territory_id like s.territory_id
+;
+
+
+
+
 
 
 -- ------ --

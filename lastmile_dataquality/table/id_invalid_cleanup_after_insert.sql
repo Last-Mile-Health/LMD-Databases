fe01bab44129da241b4e04a3feadb4470a4b7ba4
Owen Eddins <oeddins@lastmilehealth.org>
2019-11-07 15:50:11 -0700

    feat: de_case_scenario_2

--- lastmile_dataquality/table/id_invalid_cleanup_after_insert.sql
+++ lastmile_dataquality/table/id_invalid_cleanup_after_insert.sql
@@ -9,7 +9,32 @@ begin
 
 case
 
-    -- 1. de_case_scenario ----------------------------------------------------
+    -- 1. de_case_scenario_2 ----------------------------------------------------
+
+    when  trim( new.table_name )  like 'de_case_scenario_2'               and 
+          trim( new.id_type )     like 'chss'                           and 
+          trim( new.id_name )     like 'chss_id'                        and 
+          not ( new.id_repair is null )                                 and
+          not ( new.pk_id     is null )
+         
+    then
+          update lastmile_upload.de_case_scenario_2 as u
+              set u.chss_id_inserted  = new.id_repair
+          where u.de_case_scenario_id = new.pk_id;
+    
+         
+    when  trim( new.table_name )  like 'de_case_scenario_2'               and 
+          trim( new.id_type )     like 'cha'                            and 
+          trim( new.id_name )     like 'cha_id'                         and 
+          not ( new.id_repair is null )                                 and
+          not ( new.pk_id     is null )
+         
+    then
+          update lastmile_upload.de_case_scenario_2 as u
+              set u.cha_id_inserted   = new.id_repair
+          where u.de_case_scenario_id = new.pk_id;
+     
+    -- 2. de_case_scenario ----------------------------------------------------
 
     when  trim( new.table_name )  like 'de_case_scenario'               and 
           trim( new.id_type )     like 'chss'                           and 
@@ -35,7 +60,7 @@ case
           where u.de_case_scenario_id = new.pk_id;
      
 
-    -- 2.  ------------------------------------------
+    -- 3.  ------------------------------------------
 
     when  trim( new.table_name )  like 'de_chaHouseholdRegistration'    and 
           trim( new.id_type )     like 'chss'                           and 
@@ -61,7 +86,7 @@ case
           where u.chaHouseholdRegistrationID = new.pk_id;
      
 
-    -- 3. CHA Monthly Service Report ------------------------------------------
+    -- 4. CHA Monthly Service Report ------------------------------------------
     
     when  trim( new.table_name )  like 'de_cha_monthly_service_report'  and 
           trim( new.id_type )     like 'chss'                           and 
@@ -87,7 +112,7 @@ case
           where u.cha_monthly_service_report_id = new.pk_id;
      
  
-    -- 4. de_chss_commodity_distribution ---------------------------------------
+    -- 5. de_chss_commodity_distribution ---------------------------------------
  
     when  trim( new.table_name )  like 'de_chss_commodity_distribution' and 
           trim( new.id_type )     like 'chss'                           and 
@@ -101,7 +126,7 @@ case
           where u.chss_commodity_distribution_id = new.pk_id;
     
   
-    -- 5. CHSS Monthly Service Report ------------------------------------------
+    -- 6. CHSS Monthly Service Report ------------------------------------------
           
     when  trim( new.table_name )  like 'de_chss_monthly_service_report' and 
           trim( new.id_type )     like 'chss'                           and 
@@ -283,7 +308,7 @@ case
           where u.chss_monthly_service_report_id = new.pk_id;
    
          
-    -- 6. odk_chaRestock  ------------------------------------------
+    -- 7. odk_chaRestock  ------------------------------------------
     
     when  trim( new.table_name )  like 'odk_chaRestock'                 and 
           trim( new.id_type )     like 'chss'                           and 
@@ -333,7 +358,7 @@ case
           where u.chaRestockID = new.pk_id;
       
     
-    -- 7. odk_routineVisit ------------------------------------------
+    -- 8. odk_routineVisit ------------------------------------------
      
     when  trim( new.table_name )  like 'odk_routineVisit'               and 
           trim( new.id_type )     like 'cha'                            and 
@@ -347,7 +372,7 @@ case
           where u.routineVisitID = new.pk_id;
      
    
-    -- 8. odk_sickChildForm ------------------------------------------
+    -- 9. odk_sickChildForm ------------------------------------------
 
     when  trim( new.table_name )  like 'odk_sickChildForm'        and 
           trim( new.id_type )     like 'cha'                      and 
@@ -361,7 +386,7 @@ case
           where u.sickChildFormID = new.pk_id;
        
     
-    -- 9. odk_supervisionVisitLog ------------------------------------------
+    -- 10. odk_supervisionVisitLog ------------------------------------------
     
     when  trim( new.table_name )  like 'odk_supervisionVisitLog'        and 
           trim( new.id_type )     like 'chss'                           and 
@@ -388,7 +413,7 @@ case
           
           
         
-    -- 10. odk_vaccineTracker  ------------------------------------------
+    -- 11. odk_vaccineTracker  ------------------------------------------
     
     when  trim( new.table_name )  like 'odk_vaccineTracker'             and 
           trim( new.id_type )     like 'chss'                           and 
@@ -414,7 +439,7 @@ case
           where u.vaccineTrackerID = new.pk_id;
           
           
-    -- 11. odk_QAOSupervisionChecklistForm ------------------------------------------
+    -- 12. odk_QAOSupervisionChecklistForm ------------------------------------------
         
     
     when  trim( new.table_name )  like 'odk_QAOSupervisionChecklistForm'  and 

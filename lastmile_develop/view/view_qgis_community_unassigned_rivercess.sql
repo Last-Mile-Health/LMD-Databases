1fbacb38a301fe523675ebebfdf695579d59927f
Owen Eddins <oeddins@lastmilehealth.org>
2019-11-11 17:02:57 -0700

    feat: archived commuities excluded

--- lastmile_develop/view/view_qgis_community_unassigned_rivercess.sql
+++ lastmile_develop/view/view_qgis_community_unassigned_rivercess.sql
@@ -27,6 +27,7 @@ select
 
 from lastmile_cha.community
 where     ( community_id between 500 and 1999 ) and 
+          ( archived = 0 ) and
       not ( 
             community_id in ( -- build list of community IDs that are assigned to position IDs in Rivercess
                               select community_id

49cf77f0d700ffb4d6d6c0eb4742e49f8217f884
Owen Eddins <oeddins@lastmilehealth.org>
2019-12-04 12:08:24 -0700

    feat: 1st cut of view to extract GPS (x,y) points for position ID from supervision visit log form

---
+++ lastmile_report/view/view_supervision_visit_log_position_id_gps.sql
@@ -0,0 +1,16 @@
+use lastmile_report;
+
+drop view if exists lastmile_report.view_supervision_visit_log_position_id_gps;
+
+create view lastmile_report.view_supervision_visit_log_position_id_gps as
+
+select
+      trim( supervisedCHAID )                                                                       as position_id,
+      communityID                                                                                   as community_id,
+      -- gpsCoordinate,
+      trim( substring_index( trim( substring_index( trim( gpsCoordinate ), ' ', 2 ) ), ' ', -1 ) )  as x,
+      trim( substring_index( trim( gpsCoordinate ), ' ', 1 ) ) as y
+      
+from lastmile_upload.odk_supervisionVisitLog
+where not ( ( gpsCoordinate is null ) or ( trim( gpsCoordinate ) like '' ) )
+;

b4f2881b6e2fddfdd3ab3596df48fd770bf36138
Owen Eddins <oeddins@lastmilehealth.org>
2020-02-06 08:34:13 -0700

    bug: self-join not working properly, recoded.

--- lastmile_cha/view/view_history_position_person_first.sql
+++ lastmile_cha/view/view_history_position_person_first.sql
@@ -3,18 +3,11 @@ use lastmile_cha;
 drop view if exists view_history_position_person_first;
 
 create view view_history_position_person_first as
-
 select
-      pr1.person_id,
-      trim( pr1.position_id )     as position_id,
-      pr1.begin_date,
-      pr1.end_date
-     
-from position_person as pr1
-    left outer join position_person as pr2 on  ( pr1.person_id  = pr2.person_id   ) and 
-                                               ( pr1.begin_date < pr2.begin_date  )
-group by  pr1.person_id
-having    count( * ) >= 1
+      person_id,
+      min( begin_date ) as begin_date
+from lastmile_cha.position_person
+group by person_id
 ;
 
 

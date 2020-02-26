78d19a3c615376f7309318b734cda2829237a839
Owen Eddins <oeddins@lastmilehealth.org>
2020-02-25 17:53:52 -0700

    bug: fixes to lastmile_cha views

--- lastmile_cha/view/view_history_position_person_first.sql
+++ lastmile_cha/view/view_history_position_person_first.sql
@@ -1,13 +1,20 @@
 use lastmile_cha;
 
-drop view if exists view_history_position_person_first;
+drop view if exists lastmile_cha.view_history_position_person_first;
 
-create view view_history_position_person_first as
+create view lastmile_cha.view_history_position_person_first as
 select
-      person_id,
-      min( begin_date ) as begin_date
-from lastmile_cha.position_person
-group by person_id
+      pr.person_id,
+      substring_index( group_concat( trim( pr.position_id ) order by pr.begin_date asc separator ',' ), ',', 1 ) as position_id,
+      min( pr.begin_date )  as begin_date,
+      
+      if( 
+          substring_index( group_concat( coalesce( pr.end_date, 'null' ) order by pr.begin_date asc separator ',' ), ',', 1 ) like 'null', 
+          null,  
+          substring_index( group_concat( pr.end_date order by pr.begin_date asc separator ',' ), ',', 1 )
+      )
+      as end_date
+    
+from lastmile_cha.position_person as pr
+group by pr.person_id
 ;
-
-
--- lastmile_cha/view/view_history_position_person_last.sql
+++ lastmile_cha/view/view_history_position_person_last.sql
@@ -3,7 +3,23 @@ use lastmile_cha;
 drop view if exists view_history_position_person_last;
 
 create view view_history_position_person_last as
+select
+      pr.person_id,
+      substring_index( group_concat( trim( pr.position_id ) order by pr.begin_date desc separator ',' ), ',', 1 ) as position_id,
+      max( pr.begin_date )  as begin_date,
+      
+      if( 
+          substring_index( group_concat( coalesce( pr.end_date, 'null' ) order by pr.begin_date desc separator ',' ), ',', 1 ) like 'null', 
+          null,  
+          substring_index( group_concat( pr.end_date order by pr.begin_date desc separator ',' ), ',', 1 )
+      )
+      as end_date
+      
+from position_person as pr
+group by pr.person_id
+;
 
+/*
 select
       pr1.person_id,
       trim( pr1.position_id ) as position_id,
@@ -17,4 +33,4 @@ group by  pr1.person_id
 having    count( * ) >= 1
 ;
 
-
+*/

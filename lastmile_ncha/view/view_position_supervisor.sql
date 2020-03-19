143d55c45b90f913328b0255fdf93979832b0d0c
Owen Eddins <oeddins@lastmilehealth.org>
2020-03-19 15:17:45 -0600

    feat: add meta_insert_date_time

--- lastmile_ncha/view/view_position_supervisor.sql
+++ lastmile_ncha/view/view_position_supervisor.sql
@@ -9,7 +9,8 @@ select
       ps.position_supervisor_id_pk, 
       psl.position_id_last          as chss_position_id,
       ps.begin_date, 
-      ps.end_date
+      ps.end_date,
+      ps.meta_insert_date_time
 from lastmile_ncha.position_supervisor as ps
     left outer join lastmile_ncha.view_history_position_id_last       as pl   on ps.position_id_pk            = pl.position_id_pk
     left outer join lastmile_ncha.view_history_chss_position_id_last  as psl  on ps.position_supervisor_id_pk = psl.position_id_pk

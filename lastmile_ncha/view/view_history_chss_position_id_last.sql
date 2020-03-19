88ec778d2cbc1d37c38e1f4b12fea05e8ac4829e
Owen Eddins <oeddins@lastmilehealth.org>
2020-03-19 15:15:19 -0600

    feat: ncha views for dimension_position table

---
+++ lastmile_ncha/view/view_history_chss_position_id_last.sql
@@ -0,0 +1,10 @@
+use lastmile_ncha;
+
+drop view if exists lastmile_ncha.view_history_chss_position_id_last;
+
+create view lastmile_ncha.view_history_chss_position_id_last as
+select
+      position_id_pk,
+      substring_index( trim( group_concat( distinct position_id order by position_id_begin_date desc separator ',' ) ), ',', 1 ) as position_id_last
+from lastmile_ncha.view_history_person_position_chss
+group by position_id_pk
---
+++ lastmile_ncha/view/view_position_supervisor.sql
@@ -0,0 +1,16 @@
+use lastmile_ncha;
+
+drop view if exists lastmile_ncha.view_position_supervisor;
+
+create view lastmile_ncha.view_position_supervisor as 
+select 
+      ps.position_id_pk, 
+      pl.position_id_last           as position_id,
+      ps.position_supervisor_id_pk, 
+      psl.position_id_last          as chss_position_id,
+      ps.begin_date, 
+      ps.end_date
+from lastmile_ncha.position_supervisor as ps
+    left outer join lastmile_ncha.view_history_position_id_last       as pl   on ps.position_id_pk            = pl.position_id_pk
+    left outer join lastmile_ncha.view_history_chss_position_id_last  as psl  on ps.position_supervisor_id_pk = psl.position_id_pk
+;

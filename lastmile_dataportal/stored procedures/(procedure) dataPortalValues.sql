a55cc7ceef587367523d16b1951ca78221dad57c
Owen Eddins <oeddins@lastmilehealth.org>
2020-07-13 13:52:06 -0600

    feat: recode ind_id 305, 418 Supervision Visit to make them more robust.

--- lastmile_dataportal/stored procedures/(procedure) dataPortalValues.sql
+++ lastmile_dataportal/stored procedures/(procedure) dataPortalValues.sql
@@ -2029,16 +2029,33 @@ group by v.qao_position_id
 
 
 -- 305. Percent of expected CHSS mHealth supervision visit logs received
--- !!!!! This and certain other queries should be left-joined to a table of "expected counties" so that zeros are inserted
--- !!!!! Note: this currently does not calculate figures for GG-UNICEF !!!!!
-REPLACE INTO lastmile_dataportal.tbl_values (`ind_id`,`territory_id`,`period_id`,`month`,`year`,`value`)
-SELECT 305, a.territory_id, 1, @p_month, @p_year, ROUND(COUNT(1)/(2*num_cha),3)
-FROM lastmile_report.mart_view_base_odk_supervision a LEFT JOIN `lastmile_report`.`mart_program_scale` b ON a.territory_id = b.territory_id 
-WHERE manualMonth=@p_month AND manualYear=@p_year AND a.territory_id IS NOT NULL GROUP BY territory_id
-UNION SELECT 305, '6_16', 1, @p_month, @p_year, ROUND(COUNT(1)/(2*num_cha),3)
-FROM lastmile_report.mart_view_base_odk_supervision a LEFT JOIN `lastmile_report`.`mart_program_scale` b ON '6_16' = b.territory_id
-WHERE manualMonth=@p_month AND manualYear=@p_year AND a.territory_id IS NOT NULL;
+replace into lastmile_dataportal.tbl_values ( ind_id, territory_id, period_id, `month`, `year`, `value` )
+select 305, a.territory_id, 1, @p_month, @p_year, round( coalesce( a.number_visit, 0 ) / ( m.num_cha * 2 ), 3 ) as report_rate
+from (
+      -- Filter the reports based on year and month and territory_id being not null
+      select s.territory_id, count( * ) number_visit
+      from lastmile_report.mart_view_base_odk_supervision as s
+      where s.manualMonth = @p_month and s.manualYear = @p_year and not ( s.territory_id is null )
+      group by s.territory_id
+            
+) as a
+    left outer join lastmile_report.mart_program_scale as m on a.territory_id like m.territory_id 
+where not ( a.territory_id is null )
 
+union all
+
+select 305, '6_16', 1, @p_month, @p_year, round( sum( coalesce( a.number_visit, 0 ) ) / ( sum( coalesce( m.num_cha, 0 ) ) * 2 ), 3 ) as report_rate
+from (
+      -- Step 1. Filter the reports based on year and month and territory_id being not null
+      select s.territory_id, count( * ) number_visit
+      from lastmile_report.mart_view_base_odk_supervision as s
+      where s.manualMonth = @p_month and s.manualYear = @p_year and not ( s.territory_id is null )
+      group by s.territory_id
+            
+) as a
+    left outer join lastmile_report.mart_program_scale as m on a.territory_id like m.territory_id 
+where not ( a.territory_id is null )
+;
 
 -- 307. Percent of CHAs stocked out of Microlut
 REPLACE INTO lastmile_dataportal.tbl_values (`ind_id`,`territory_id`,`period_id`,`month`,`year`,`value`)
@@ -3647,81 +3664,40 @@ WHERE month_reported = @p_month AND
       county_id IS NOT NULL
 ;
 
-/*  418. Percent of CHAs who received a supervision visit in the past month. 
-    Notes:  For Grand Gedeh, the UNICEF CHAs are not being counted in the denominator.  To add them in, remove the
-            the "cohort is null" from the where clause (two places) below.
 
-*/
-replace into lastmile_dataportal.tbl_values ( ind_id , territory_id, period_id, `month`, `year`, value )
-select
-      418,
-      case 
-          when a.county like 'Rivercess'    then '1_14'
-          when a.county like 'Grand Gedeh'  then '6_31'
-          when a.county like 'Grand Bassa'  then '1_4'
-          else null
-      end  as territory_id, 
-      1, 
-      @p_month, 
-      @p_year,
-      round( count( a.position_id_supervision ) / count( a.position_id), 2 ) as supervision_rate 
-from ( 
-        select c.county, c.position_id, s.position_id_supervision
-        from lastmile_report.data_mart_snapshot_position_cha as c
-            left outer join ( 
-                              select supervisedCHAID   as position_id_supervision
-                              from lastmile_upload.odk_supervisionVisitLog
-                              where                          
-                                    ( meta_fabricated = 0 )           and
-                                    ( supervisionAttendance = 1 )     and -- only count record if this flag is set.
-                                    ( month( manualDate ) = @p_month  and year( manualDate ) = @p_year ) 
-                              group by supervisedCHAID 
-                              
-                            ) as s on c.position_id like s.position_id_supervision                   
-        where 
-              (       
-                ( c.county like 'Rivercess'   ) or
-                ( c.county like 'Grand Bassa' ) or
-                ( c.county like 'Grand Gedeh' and c.cohort is null  )
-              ) 
-              and
-              ( ( month( c.snapshot_date ) =  @p_month ) and ( year( c.snapshot_date ) = @p_year ) ) 
-      
+-- 418. Percent of CHAs who received a supervision visit in the past month.
+replace into lastmile_dataportal.tbl_values ( ind_id, territory_id, period_id, `month`, `year`, `value` )
+select 418, a.territory_id, 1, @p_month, @p_year, round( coalesce( a.number_visit, 0 ) / coalesce( m.num_cha, 0 ), 3 ) as report_rate
+from (
+      select b.territory_id, count( * ) as number_visit
+      from (
+              -- Filter the reports based on year and month and territory_id being not null
+              select s.territory_id, s.supervisedCHAID as cha_id
+              from lastmile_report.mart_view_base_odk_supervision as s
+              where s.manualMonth = @p_month and s.manualYear = @p_year and not ( s.territory_id is null )
+              group by s.territory_id, s.supervisedCHAID
+ 
+      ) as b
+    group by b.territory_id
 ) as a
-group by a.county
+left outer join lastmile_report.mart_program_scale as m on a.territory_id like m.territory_id 
 
 union all
 
-select
-      418,
-      '6_16' as territory_id,
-      1, 
-      @p_month, 
-      @p_year,
-      round( count( a.position_id_supervision ) / count( a.position_id ), 2 ) as supervision_rate 
-from ( 
-        select c.county, c.position_id, s.position_id_supervision
-        from lastmile_report.data_mart_snapshot_position_cha as c
-            left outer join ( 
-                              select supervisedCHAID   as position_id_supervision
-                              from lastmile_upload.odk_supervisionVisitLog
-                              where                          
-                                    ( meta_fabricated = 0 )           and
-                                    ( supervisionAttendance = 1 )     and -- only count record if this flag is set.
-                                    ( month( manualDate ) = @p_month  and year( manualDate ) = @p_year ) 
-                              group by supervisedCHAID 
-                              
-                            ) as s on c.position_id like s.position_id_supervision                   
-        where
-              (       
-                ( c.county like 'Rivercess'   ) or
-                ( c.county like 'Grand Bassa' ) or
-                ( c.county like 'Grand Gedeh' and c.cohort is null  )
-              ) 
-              and
-              ( ( month( c.snapshot_date ) =  @p_month ) and ( year( c.snapshot_date ) = @p_year ) ) 
-      
+select 418, a.territory_id, 1, @p_month, @p_year, round( sum( coalesce( a.number_visit, 0 ) ) / sum( coalesce( m.num_cha, 0 ) ), 3 ) as report_rate
+from (
+      select b.territory_id, count( * ) as number_visit
+      from (
+              -- Filter the reports based on year and month and territory_id being not null
+              select s.territory_id, s.supervisedCHAID as cha_id
+              from lastmile_report.mart_view_base_odk_supervision as s
+              where s.manualMonth = @p_month and s.manualYear = @p_year and not ( s.territory_id is null )
+              group by s.territory_id, s.supervisedCHAID
+ 
+      ) as b
+    group by b.territory_id
 ) as a
+left outer join lastmile_report.mart_program_scale as m on a.territory_id like m.territory_id 
 ;
 
 
